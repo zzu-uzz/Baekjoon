f09fac2def7cd06948905188bb77c236e3e0dc61
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-12 23:39:24 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(A + B) + "\n");
+        bw.write(String.valueOf(A - B) + "\n");
+        bw.write(String.valueOf(A * B) + "\n");
+        bw.write(String.valueOf(A / B) + "\n");
+        bw.write(String.valueOf(A % B));
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
