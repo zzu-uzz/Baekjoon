76cc4cd19651a95059ee30b226107f044437ab2f
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-13 19:04:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        bw.write(String.valueOf(A + B + C));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
