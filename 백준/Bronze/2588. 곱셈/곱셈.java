99c0e31455c38b898a7bfe313798e37e75d7032a
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-13 18:42:00 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf((A * (B % 10)) + "\n"));
+        bw.write(String.valueOf((A * ((B % 100) / 10)) + "\n"));
+        bw.write(String.valueOf((A * (B / 100))) + "\n");
+        bw.write(String.valueOf((A * B)));
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
