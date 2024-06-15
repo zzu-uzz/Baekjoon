1813086c5294be1cda7b0e72496def61ff83e75d
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-16 00:49:21 +0900

    [Bronze V] Title: A+B - 3, Time: 140 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write(String.valueOf((A + B) + "\n"));
+        }
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
