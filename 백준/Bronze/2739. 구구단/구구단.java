5532b1d8f6515f1b2487e23013a0385a7b05e1ed
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-16 00:37:40 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16168 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,11 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i < 10; i++) {
             bw.write(String.valueOf(N + " * " + i + " = " + (N * i) + "\n"));
