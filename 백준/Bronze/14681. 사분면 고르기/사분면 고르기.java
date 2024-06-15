3582b1181d2efa9d0f3a2f8e2a0aa40db7d6c91d
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-15 21:51:25 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if (x > 0 && y > 0) {
+            bw.write("1");
+        } else if (x < 0 && y > 0) {
+            bw.write("2");
+        } else if (x < 0 && y < 0) {
+            bw.write("3");
+        } else if (x > 0 && y < 0) {
+            bw.write("4");
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
