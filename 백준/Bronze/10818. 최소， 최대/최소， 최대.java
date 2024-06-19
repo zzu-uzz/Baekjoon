165b48860bd759b8801007ecf9e102da1447661f
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-19 23:44:31 +0900

    [Bronze III] Title: 최소, 최대, Time: 472 ms, Memory: 92260 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = arr[0];
+        for(int i = 0; i <arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        bw.write(String.valueOf(min + " " + max));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
