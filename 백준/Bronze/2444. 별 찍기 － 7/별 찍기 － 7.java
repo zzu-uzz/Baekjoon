897a7711e5322d404c292f52df7b0faa57afa72a
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-06-27 23:27:04 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 116 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = 1; j <= N - i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
