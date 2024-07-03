9a82f7fbe41e2678ca394bcdccd7f83ae63ac598
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-07-03 22:45:46 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 2;
+
+        if (N == 1) {
+            bw.write(String.valueOf(1));
+        } else {
+            while (range <= N) {
+                range = range + (6 * count);
+                count++;
+            }
+            bw.write(String.valueOf(count));
+        }
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
