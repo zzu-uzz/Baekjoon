cd77767f2b8a270b827b8088ad0a9f329b26cd12
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-07-04 21:41:09 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) {
+            day++;
+        }
+        bw.write(String.valueOf(day));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
