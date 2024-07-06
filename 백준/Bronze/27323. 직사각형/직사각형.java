5ee5ac5dbdd09a5b0891ec417b91e50e85a11c96
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-07-06 15:45:10 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(A * B));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
