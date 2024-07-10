334694b96d6a06718112bd274352729142a35464
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-07-11 00:46:54 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 116 ms, Memory: 15776 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Long n = Long.parseLong(br.readLine());
+
+        bw.write((n * n) + "\n");
+        bw.write('2');
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
