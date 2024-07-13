fc87b3520baf9da244f848fd2716cfc9fae230fc
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-07-13 23:57:21 +0900

    [Bronze II] Title: 분해합, Time: 156 ms, Memory: 17072 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while (number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if (sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+        bw.write(result + "\n");
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
